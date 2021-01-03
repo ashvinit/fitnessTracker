2834139e127cf9f1eabe8c78acfb1bb1462b0bf7
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-03 17:14:39 -0500

    created workout.js mongoose schema

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    cardio: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            distance: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            }
+        }
+    ],
+    resistance: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            weight: {
+                type: Number
+            }, 
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            }, 
+            duration: {
+                type: Number
+            }
+        }
+    ]
+});  
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
