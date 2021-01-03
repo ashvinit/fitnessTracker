78be657d6cb22fcd8a8e6a8cc5ddd0a0a7758afa
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-03 17:31:38 -0500

    added route to stats.html

--- routes/html.js
+++ routes/html.js
@@ -9,3 +9,6 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
